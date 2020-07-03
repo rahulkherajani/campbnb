1041758f8c9263474fae09525efb64d910fd87ec
Rahul Kherajani <rahul.kherajani20@gmail.com>
2020-07-03 10:39:27 +0530

    PORT and IP corrected

--- app.js
+++ app.js
@@ -71,6 +71,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 //Listen to Requests
-app.listen(3000, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
   console.log("The CampBnB Server Has Started!");
 });
